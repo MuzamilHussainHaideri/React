929794729a9a6f7db2c6c1c15d80dae7ed9f67dd
MuzamilHussainHaideri <haiderimuzamil@gmail.com>
2025-10-28 17:43:42 +0500

    Added counter to see state working.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import { Greet } from './components/Greet';
+import { Counter } from './components/Counter';
 
 function App() {
   return (
-    <div className="App">
-      <Greet name = "Muzamil" age = {20} />
-      <Greet name = "Husain" age = {21} />
-      <Greet name = "Hideri" age = {22} />
+    <div>
+      <div className="App">
+        <Greet name = "Muzamil" age = {20} />
+        <Greet name = "Husain" age = {21} />
+        <Greet name = "Hideri" age = {22} />
+      </div>
+      <div>
+        <Counter />
+      </div>
     </div>
   );
 }
