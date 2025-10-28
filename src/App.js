b8d428d4c51d65f898f56c0ae05a62cfa5cf7ebe
MuzamilHussainHaideri <haiderimuzamil@gmail.com>
2025-10-28 11:53:41 +0500

    Added First React component.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World!
-        </p>
-      </header>
+      <Greet />
     </div>
   );
 }
